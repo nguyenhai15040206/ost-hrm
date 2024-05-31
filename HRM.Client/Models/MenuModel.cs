1d3aaa2745d7113873e0a591fc21bbf014027963
Hai Nguyen <nguyenhai15040206@gmail.com>
2024-05-31 14:16:20 +0700

    Add project files.

---
+++ HRM.Client/Models/MenuModel.cs
@@ -0,0 +1,17 @@
+﻿namespace HRM.Client.Models
+{
+    public class MenuModel
+    {
+        public int Id { get; set; }
+        public string? MenuCode { get; set; }
+        public string? MenuName { get; set; }
+        public string? Icon { get; set; }
+        public string? IconType { get; set; }
+        public string? Link { get; set; }
+        public string? Controller { get; set; }
+        public string? ParentCode { get; set; }
+        public int Level { get; set; }
+        public bool IsVisible { get; set; }
+        public int OrdinalNumber { get; set; }
+    }
+}
