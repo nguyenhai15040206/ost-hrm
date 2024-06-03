e807293fcbddb21c3ce3a9550684f988aa53174d
Hai Nguyen <nguyenhai15040206@gmail.com>
2024-06-03 17:40:23 +0700

    feat: khen thưởng - phụ cấp

---
+++ HRM.Client/Components/Pages/Salary/BonusAndAllowancePage.razor.cs
@@ -0,0 +1,72 @@
+﻿using HRM.Client.Models;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace HRM.Client.Components.Pages.Salary
+{
+    public partial class BonusAndAllowancePage
+    {
+        public List<ComboboxModel>? ListUsers { get; set; }
+        public BonusAndAllowanceModel SelectModel { get; set; } = new BonusAndAllowanceModel();
+        public List<CollectTaxesArrearModel> ListGrids { get; set; } = new List<CollectTaxesArrearModel>();
+        public EditContext? _EditContext { get; set; }
+        public DateTime TuNgay { get; set; } = DateTime.Now;
+        public DateTime DenNgay { get; set; } = DateTime.Now.AddDays(3);
+        public int ActiveTabIndex { get; set; } = 0;
+
+        public List<ComboboxModel>? ListLoaiThuong { get; set; }
+        public List<ComboboxModel>? ListTinhTrang { get; set; }
+        public List<ComboboxModel>? ListThoiDiemChiTra { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            ListGrids = new List<CollectTaxesArrearModel>();
+            ListGrids.Add(new CollectTaxesArrearModel()
+            {
+                maNhanVien = "NV001",
+                tenNhanVien = "Nguyễn Văn A",
+                phongBan = "Phòng ban IT",
+                thangTrichNop = 4,
+                namTrichNop = 2024,
+                thangTruyThu = 4,
+                namTruyThu = 2024,
+                luongCoBan = 5000000,
+                daTruyThu = true
+            });
+            ListGrids.Add(new CollectTaxesArrearModel()
+            {
+                maNhanVien = "NV002",
+                tenNhanVien = "Nguyễn Văn B",
+                phongBan = "Phòng ban IT",
+                thangTrichNop = 4,
+                namTrichNop = 2024,
+                thangTruyThu = 4,
+                namTruyThu = 2024,
+                luongCoBan = 5000000,
+                daTruyThu = true
+            });
+            createDataCombo();
+            _EditContext = new EditContext(SelectModel);
+        }
+
+
+        private void createDataCombo()
+        {
+            ListLoaiThuong = new List<ComboboxModel>();
+            ListLoaiThuong.Add(new ComboboxModel() { code = "LT001", name = "Quỹ khen thưởng đơn vị khôi phục" });
+            ListLoaiThuong.Add(new ComboboxModel() { code = "LT002", name = "Thuế tháng 13" });
+            SelectModel.loaiThuong = "LT001";
+
+            ListTinhTrang = new List<ComboboxModel>();
+            ListTinhTrang.Add(new ComboboxModel() { code = "TT001", name = "Tạo mới" });
+            ListTinhTrang.Add(new ComboboxModel() { code = "TT002", name = "Đã duyệt" });
+            ListTinhTrang.Add(new ComboboxModel() { code = "TT003", name = "Hủy" });
+            SelectModel.tinhTrang = "TT001";
+
+            ListThoiDiemChiTra = new List<ComboboxModel>();
+            ListThoiDiemChiTra.Add(new ComboboxModel() { code = "TT001", name = "Lương kỳ 1" });
+            ListThoiDiemChiTra.Add(new ComboboxModel() { code = "TT002", name = "Lương kỳ 2" });
+            SelectModel.thoiDiemChiTra = "TT002";
+        }
+    }
+}
---
+++ HRM.Client/Models/BonusAndAllowanceModel.cs
@@ -0,0 +1,21 @@
+﻿namespace HRM.Client.Models
+{
+    public class BonusAndAllowanceModel
+    {
+        public string loaiThuong { get; set; }
+        public string tenNhanVien { get; set; }
+        public string soPhieu { get; set; }
+        public DateTime ngayLap { get; set; } = DateTime.Now;
+        public string tinhTrang { get; set; }
+        public string soQuyetDinh { get; set; }
+        public DateTime ngayKi { get; set; } = DateTime.Now;
+        public string nguoiLap { get; set; }
+        public string nguoiKi { get; set; }
+        public decimal tongTienThuong { get; set; }
+        public DateTime thangThuong { get; set; } = DateTime.Now;
+        public DateTime thangChiTra { get; set; } = DateTime.Now;
+        public DateTime thangTinhThue { get; set; } = DateTime.Now;
+        public string thoiDiemChiTra { get; set; }
+        public string noiDung { get; set; }
+    }
+}
